d13856b08cf9397a9177673654dee882f5fdb8a0
Kevin Martensson <kevinmartensson@gmail.com>
2019-09-24 19:31:02 +0200

    Require Node.js 10

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 const got = require('got');
-const twitterText = require('twitter-text');
+const instagramUser = require('instagram-user');
+const {extractHashtags, extractMentions} = require('twitter-text');
 
 const QUERY_HASH = '58b6785bea111c67129decbe6a448951';
 
-const filterPosts = (posts, options) => posts.filter(post => {
+const filterPosts = (posts = [], options = {}) => posts.filter(post => {
 	let pass = true;
 
 	if (typeof options.filter === 'function') {
@@ -12,32 +13,40 @@ const filterPosts = (posts, options) => posts.filter(post => {
 	}
 
 	if (pass && Array.isArray(options.hashtags)) {
-		pass = options.hashtags.every(hashtag => twitterText.extractHashtags(post.text).indexOf(hashtag) !== -1);
+		pass = options.hashtags.every(hashtag => extractHashtags(post.text).includes(hashtag));
 	}
 
 	if (pass && Array.isArray(options.mentions)) {
-		pass = options.mentions.every(mention => twitterText.extractMentions(post.text).indexOf(mention) !== -1);
+		pass = options.mentions.every(mention => extractMentions(post.text).includes(mention));
 	}
 
 	return pass;
 });
 
-const transformPosts = posts => posts.map(post => Object.assign(post.node, {
-	comments: post.node.edge_media_to_comment ? post.node.edge_media_to_comment.count : 0,
-	likes: post.node.edge_liked_by ? post.node.edge_liked_by.count : 0,
-	media: post.node.display_url,
-	text: post.node.edge_media_to_caption.edges.map(edge => edge.node.text).join(''),
-	time: post.node.taken_at_timestamp,
-	type: post.node.is_video ? 'video' : 'image',
-	username: post.node.owner.username
+const transformPosts = (posts = []) => posts.map(({node}) => ({
+	...node,
+	comments: node.edge_media_to_comment ? node.edge_media_to_comment.count : 0,
+	likes: node.edge_liked_by ? node.edge_liked_by.count : 0,
+	media: node.display_url,
+	text: node.edge_media_to_caption.edges.map(({node: {text}}) => text).join(''),
+	time: node.taken_at_timestamp,
+	type: node.is_video ? 'video' : 'image',
+	username: node.owner.username
 }));
 
-const fetchPosts = (id, posts, cursor, options) => {
+const fetchPosts = async (id, posts = [], cursor, options = {}) => {
 	const postsLeft = options.count - posts.length;
-
-	return got('https://www.instagram.com/graphql/query', {
-		json: true,
-		query: {
+	const {
+		data: {
+			user: {
+				edge_owner_to_timeline_media: {
+					edges: currentPosts,
+					page_info: {end_cursor, has_next_page}
+				}
+			}
+		}
+	} = await got('https://www.instagram.com/graphql/query', {
+		searchParams: {
 			query_hash: QUERY_HASH,
 			variables: JSON.stringify({
 				after: cursor,
@@ -45,40 +54,38 @@ const fetchPosts = (id, posts, cursor, options) => {
 				id
 			})
 		}
-	}).then(response => {
-		const currentPosts = response.body.data.user.edge_owner_to_timeline_media.edges;
-		const info = response.body.data.user.edge_owner_to_timeline_media.page_info;
-		const filteredPosts = filterPosts(transformPosts(currentPosts), options);
+	}).json();
 
-		if (info.has_next_page && (posts.length + filteredPosts.length) < options.count) {
-			return fetchPosts(
-				id,
-				posts.concat(filteredPosts),
-				info.end_cursor,
-				options
-			);
-		}
+	const filteredPosts = filterPosts(transformPosts(currentPosts), options);
 
-		return posts.concat(filteredPosts.slice(0, postsLeft));
-	});
+	if (has_next_page && (posts.length + filteredPosts.length) < options.count) {
+		return fetchPosts(
+			id,
+			posts.concat(filteredPosts),
+			end_cursor,
+			options
+		);
+	}
+
+	return posts.concat(filteredPosts.slice(0, postsLeft));
 };
 
-module.exports = (user, opts) => {
-	opts = Object.assign({count: 20}, opts);
+module.exports = async (username, options = {}) => {
+	options = {count: 20, ...options};
 
-	return got(`https://instagram.com/${user}`, {
-		json: true,
-		query: {__a: 1}
-	}).then(response => {
-		const currentUser = response.body.graphql.user;
-		const currentPosts = currentUser.edge_owner_to_timeline_media.edges;
-		const info = currentUser.edge_owner_to_timeline_media.page_info;
-		const filteredPosts = filterPosts(transformPosts(currentPosts), opts);
+	const {
+		edge_owner_to_timeline_media: {
+			edges: currentPosts,
+			page_info: {end_cursor: endCursor, has_next_page}
+		},
+		id
+	} = await instagramUser(username);
 
-		if (info.has_next_page && filteredPosts.length < opts.count) {
-			return fetchPosts(currentUser.id, filteredPosts, info.end_cursor, opts);
-		}
+	const filteredPosts = filterPosts(transformPosts(currentPosts), options);
 
-		return filteredPosts.slice(0, opts.count);
-	});
+	if (has_next_page && filteredPosts.length < options.count) {
+		return fetchPosts(id, filteredPosts, endCursor, options);
+	}
+
+	return filteredPosts.slice(0, options.count);
 };
